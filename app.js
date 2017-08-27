d0cd24e910fd274873195d18089ad21946bf06fe
Sarvesh Deshmukh <sarvesh_deshmukh@yahoo.com>
2017-08-27 14:35:04 +0000

    add register route and authenticate functionality

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express               = require("express"),
     passportLocalMangoose = require("passport-local-mongoose"),
     app                   = express(),
     User                  = require("./models/user");
-
+    
 mongoose.connect("mongodb://localhost/auth_demo_app", {useMongoClient : true});
 
 app.use(require("express-session")({
@@ -16,13 +16,16 @@ app.use(require("express-session")({
 })); 
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
-app.use(passport.session);
+app.use(passport.session());
 
 passport.serializeUser(User.serializeUser());
-passport.deSerialized(User.deserializedUser());
+passport.deserializeUser(User.deserializeUser());
 
-
+//===========
+//ROUTES
+//===========
 app.get("/", function(req, res){
     res.render("home");
 });
@@ -30,6 +33,28 @@ app.get("/", function(req, res){
 app.get("/secret", function(req, res){
     res.render("secret"); 
 });
+
+//Auth routes
+
+//Show sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+})
+
+//Handling user sign up
+app.post("/register", function(req, res){
+    console.log("User name is "+ req.body.username);
+   User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+       if(err){
+           console.log(err);
+       }
+           passport.authenticate("local")(req, res, function(){
+               res.redirect("/secret");
+           });
+       
+   }); 
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started");
