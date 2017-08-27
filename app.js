5c58daff95c2886fde7ad88f8c37c40469b485b5
Sarvesh Deshmukh <sarvesh_deshmukh@yahoo.com>
2017-08-27 22:36:14 +0000

    add logout functionality

--- app.js
+++ app.js
@@ -31,20 +31,20 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret"); 
 });
 
 //Auth routes
 
 //Show sign up form
-app.get("/register", function(req, res){
+app.get("/register",function(req, res){
     res.render("register");
 })
 
 //Handling user sign up
 app.post("/register", function(req, res){
-    console.log("User name is "+ req.body.username);
+    
    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
        if(err){
            console.log(err);
@@ -68,6 +68,20 @@ app.post("/login", passport.authenticate("local", {
 }), function(req, res){
     
 });
+
+//LOGOUT route
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+})
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started");
